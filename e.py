929ab40cbc546be9c6689044c7fad35574608705
Colon <kfirgoldman12@gmail.com>
2023-09-14 14:31:40 +0000

    shh u saw nothing

--- e.py
+++
@@ -1,6 +0,0 @@
-from pprint import pprint
-import CloudflareManager
-apikey = "96d308eccd656388c9da4b53975f9aef5f3ab"
-email = "kfirgoldman12@gmail.com"
-pprint(CloudflareManager.ListDomains(email, apikey).ListDomainRecordsData(
-    str(CloudflareManager.ListDomains(email, apikey).GetDomainId("tavernxkobold.xyz"))))
